ffff74aa37e1e688f8ef96b0c33f2709279540b9
Kauan <raidoka546@gmail.com>
2024-03-21 17:24:52 -0400

    logic to play songs accord to buttons

--- js/focusTimer/sounds.js
+++ js/focusTimer/sounds.js
@@ -1,4 +1,51 @@
+import state from "./state.js";
+
 export const chillSong = new Audio('../src/songs/Heart-Of-The-Ocean(chosic.com).mp3');
 export const rainySong = new Audio('../src/songs/Heart-Of-The-Ocean(chosic.com).mp3');
 export const coffeeSong = new Audio('../src/songs/Heart-Of-The-Ocean(chosic.com).mp3');
 export const campSong = new Audio('../src/songs/Heart-Of-The-Ocean(chosic.com).mp3');
+
+const chillSongButton = document.getElementById('chillSongButton')
+const rainySongButton = document.getElementById('rainySongButton')
+const coffeeSongButton = document.getElementById('coffeeSongButton')
+const campingSongButton = document.getElementById('campingSongButton')
+
+export function toggleMuteState(buttonId) {
+    const button = document.getElementById(buttonId);
+
+    if (button) {
+        button.addEventListener('click', () => {
+            if (!chillSong.paused || !rainySong.paused || !coffeeSong.paused || !campSong.paused) {
+                state.isMuted = true;
+            } else {
+                state.isMuted = !state.isMuted;
+            }
+
+            chillSong.pause();
+            rainySong.pause();
+            coffeeSong.pause();
+            campSong.pause();
+
+            if (!state.isMuted) {
+                if (buttonId === 'chillSongButton') {
+                    chillSong.play();
+                    chillSong.loop = true;
+                } else if (buttonId === 'rainySongButton') {
+                    rainySong.play();
+                    rainySong.loop = true;
+                } else if (buttonId === 'coffeeSongButton') {
+                    coffeeSong.play();
+                    coffeeSong.lop = true;
+                } else if (buttonId === 'campingSongButton') {
+                    campSong.play();
+                    campSong.loop = true;
+                }
+            }
+        });
+    }
+}
+
+toggleMuteState('chillSongButton');
+toggleMuteState('rainySongButton');
+toggleMuteState('coffeeSongButton');
+toggleMuteState('campingSongButton');
